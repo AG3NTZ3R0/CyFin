6914b50261dd2f86d1a138e4bc077a85ffc091c8
blacy <brandon.lacy.50@gmail.com>
2022-02-09 08:37:10 -0500

    Restructured directory and updated filepaths

--- stock_price/get_stock_data.py
+++ stock_price/get_stock_data.py
@@ -6,14 +6,14 @@ import yfinance as yf
 def main(start_date='2004-01-01', end_date='2022-01-01'):
     """Extract ticker symbols from both csv files and generate technical datasets."""
     # Read BUG CSV into pandas dataframe
-    bug_full_holdings_df = pd.read_csv('bug_full_holdings_20220119.csv')
+    bug_full_holdings_df = pd.read_csv('./stock_price/bug_full_holdings_20220119.csv')
     # Create list from Ticker column
     bug_tickers = bug_full_holdings_df['Unnamed: 1'].to_list()
     # Delete the headers and miscellaneous data
     bug_tickers = bug_tickers[2:-2]
 
     # Read breach CSV into pandas dataframe
-    company_metadata_df = pd.read_csv("breach_dataset/company_meta_dataset/company_metadata.csv")
+    company_metadata_df = pd.read_csv("./breach_dataset/company_meta_dataset/company_metadata.csv")
     company_metadata_tickers = company_metadata_df['symbol'].to_list()
     all_tickers = list(set(bug_tickers + company_metadata_tickers))
     all_tickers = list(map(lambda x: str(x), all_tickers))
@@ -25,7 +25,7 @@ def main(start_date='2004-01-01', end_date='2022-01-01'):
                               any(char.isdigit() for char in x) is not True, all_tickers))
 
     for ticker in all_tickers:
-        path = f"stock_price_ds/{ticker}_hist.csv"
+        path = f"./stock_price/stock_history_dataset/{ticker}_hist.csv"
         yf_object = yf.Ticker(ticker)
         hist = yf_object.history(start=start_date, end=end_date, interval='1d')
         # Normalize column names
