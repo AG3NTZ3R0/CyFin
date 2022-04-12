1dca02e7808d1cfdd695701d1610ea5303b46878
AG3NTZ3R0 <Z3R0@duck.com>
2022-04-12 17:40:05 -0400

    fix: set monthly stock data

--- stock_price/extract_stock_data.py
+++ stock_price/extract_stock_data.py
@@ -1,23 +1,32 @@
 import pandas as pd
-import wxve as x
+import yh_finance as yhf
 
 
 def main():
     """Get stock data using wxve."""
     api_key = 'YOUR_API_KEY'
 
-    bug = x.Stock('BUG', api_key)
-    bug.hist_df.to_csv(f"../CyFin/stock_price/sets/bug_etf/stock_hist/set/BUG_hist.csv")
-    bug.hist_df.to_csv(f"../CyFin/stock_price/sets/bug_etf/stock_visual/set/BUG_visual.png")
-
     bug_etf_df = pd.read_csv('../CyFin/stock_price/bug_etf_holdings.csv')
     bug_etf_symbols = bug_etf_df['Ticker'].tolist()
-    bug_etf = x.StockSet(bug_etf_symbols, api_key)
 
-    for stock in bug_etf.stocks.keys():
+    for stock in bug_etf_symbols:
         print(stock)
-        bug_etf.stocks[stock].hist_df.to_csv(f"../CyFin/stock_price/sets/bug_etf/stock_hist/element/{stock}_hist.csv")
-        bug_etf.stocks[stock].candlestick.write_image(f"../CyFin/stock_price/sets/bug_etf/stock_visual/element/{stock}_visual.png")
+        json_resp_month = yhf.get_chart(interval='1mo',
+                                        symbol=stock,
+                                        time_range='10y',
+                                        region='US',
+                                        include_pre_post='false',
+                                        use_yahoo_id='true',
+                                        include_adj_close='true',
+                                        events='div',
+                                        api_key=api_key)
+
+        month_data = pd.DataFrame()
+        month_data['date'] = pd.to_datetime(json_resp_month['chart']['result'][0]['timestamp'], unit='s').date
+        month_data['month'] = pd.to_datetime(json_resp_month['chart']['result'][0]['timestamp'], unit='s').month
+        month_data['year'] = pd.to_datetime(json_resp_month['chart']['result'][0]['timestamp'], unit='s').year
+        month_data['adjclose'] = json_resp_month['chart']['result'][0]['indicators']['adjclose'][0]['adjclose']
+        month_data.to_csv(f"../CyFin/stock_price/sets/bug_etf/stock_hist/element/{stock}_hist.csv")
 
 
 if __name__ == '__main__':
