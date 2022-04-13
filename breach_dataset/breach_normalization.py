1ef51cbd547a4081fcd1f06f8c8e3b18181fed62
Charles Lin Chargi <50467048+ch0r1es-1n-ch0rge@users.noreply.github.com>
2022-04-13 16:32:52 -0400

    committed new date parser capability
    Split initial (ex. Jan 2020) to ex. breach_month=1, breach_year=2020

--- breach_dataset/breach_normalization.py
+++ breach_dataset/breach_normalization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 
 def main():
@@ -9,10 +10,20 @@ def main():
     sector_dataframe = breach_dataframe['sector']
     method_dataframe = breach_dataframe['method']
 
+    breach_month = []
+    breach_year = []
+    for _ in date_dataframe:
+        date_split = _.split()
+        datetime_object = datetime.strptime(date_split[0], "%b")
+        month_number = datetime_object.month
+        breach_month.append(month_number)
+        breach_year.append(date_split[1])
+
     normalized_csv = pd.DataFrame({
         'company_name': org_dataframe,
         'company_sector': sector_dataframe,
-        'breach_date': date_dataframe,
+        'breach_month': breach_month,
+        'breach_year': breach_year,
         'breach_method': method_dataframe,
         'records_lost': records_dataframe
     })
